44073e9dc832f85ccd898b64c3a945646e7e972f
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2024-09-19 10:47:57 -0300

    tipos primitivos e inferencia de tipo

--- exercicios/src/fundamentos/Temperatura.java
+++ exercicios/src/fundamentos/Temperatura.java
@@ -3,10 +3,10 @@ package fundamentos;
 public class Temperatura {
 
 	public static void main(String[] args) {
-		int temperaturaF = 20;
 		final int  AJUSTE = 32;
 		final double FATOR = 5 / 9.0;
-		double celsius = (temperaturaF - AJUSTE) * FATOR;
+		int fahrenheit = 20;
+		double celsius = (fahrenheit - AJUSTE) * FATOR;
 		
 		// (F - 32) * 5/9 = C
 		System.out.println(celsius);
---
+++ exercicios/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		
+		// 1 Byte = 8 bits
+		// 
+	}
+}
